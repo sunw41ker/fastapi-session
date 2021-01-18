196753023b0811bfd5b5fb8e3cfec11e0a28f175
Max Yakovenko <mxyakovenko9@gmail.com>
2021-01-18 11:08:22 +0300

    Remove memory MemoryBackend import (#6)

--- flaskapi_session/backends/__init__.py
+++ flaskapi_session/backends/__init__.py
@@ -1,6 +1,4 @@
 # from .database import DBBackend
 from .fs import FSBackend
 from .interfaces import BackendInterface, FactoryInterface
-
-# from .memory import MemoryBackend
 from .redis import RedisBackend
