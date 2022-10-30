7e798aee887f2ffe69fd64ec029ba60415ebfe12
sunw41ker <sunw41ker@yandex.ru>
2022-10-30 16:27:06 +0300

    Bugfix/websocket request acception

--- fastapi_session/middlewares.py
+++ fastapi_session/middlewares.py
@@ -2,6 +2,7 @@ import json
 import typing
 
 from fastapi import FastAPI, HTTPException, Request, Response, status
+from fastapi.requests import HTTPConnection
 from starlette.types import Receive, Scope, Send
 
 from .exceptions import InvalidCookieException
@@ -26,11 +27,15 @@ class SessionMiddleware:
         self.strict = strict
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        if scope["type"] not in ("http", "websocket"):  # pragma: no cover
+        WEBSOCKET_TYPE = "websocket"
+        if scope["type"] not in ("http", WEBSOCKET_TYPE):  # pragma: no cover
             await self.app(scope, receive, send)
             return
 
-        request = Request(scope, receive, send)
+        if scope["type"] == WEBSOCKET_TYPE:
+            request = HTTPConnection(scope, receive)
+        else:
+            request = Request(scope, receive, send)
         scope["session"] = None
 
         if self.manager.has_cookie(request):
